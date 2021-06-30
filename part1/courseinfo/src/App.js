93c00d42740778227d0dbbc1e502c337e8477275
sh4rdu1-git <mulayshardul99@gmail.com>
2021-06-30 17:38:26 +0530

    Exercise 1.1 completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
+const Header = (props) => {
+  return (
+    <h1>{props.courseName}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <p>{props.parts[0]} {props.exercises[0]}</p>
+      <p>{props.parts[1]} {props.exercises[1]}</p>
+      <p>{props.parts[2]} {props.exercises[2]}</p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack Application Development'
   const part1 = 'Fundamentals of React'
@@ -11,17 +33,12 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      {/* For <Header />, passing courseName as prop */}
+      <Header courseName={course} />
+      {/* For <Content />, passing two props, parts and exercises, which are arrays */}
+      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
+      {/* For <Total />, passing exercises array as prop */}
+      <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   )
 }
