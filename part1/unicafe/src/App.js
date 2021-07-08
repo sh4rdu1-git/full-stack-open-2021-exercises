71347cede0dc97507e9a9e2429711dc534f263b0
sh4rdu1-git <mulayshardul99@gmail.com>
2021-07-08 15:36:07 +0530

    Exercise 1.7: unicafe step2 complete
    Application expanded to show more statistics like total number of gathered feedbacks, the average score and percentage of positive feedback

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,17 +6,27 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const incrementGood = () => setGood(good + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
+  const incrementBad = () => setBad(bad + 1)
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const percentPositive = (good / total) * 100
+
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <button onClick={incrementGood}>Good</button>
+      <button onClick={incrementNeutral}>Neutral</button>
+      <button onClick={incrementBad}>Bad</button>
 
       <h1>Statistics</h1>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {total}</p>
+      <p>Average {average}</p>
+      <p>Positive {percentPositive}%</p>
     </div>
   )
 }
