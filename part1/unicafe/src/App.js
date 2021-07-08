09d0344ed47dee9b822e6a94ffc032772e44f2f8
sh4rdu1-git <mulayshardul99@gmail.com>
2021-07-08 15:03:05 +0530

    Exercise 1.6: unicafe step1 completed (UPDATED)
    Added the features as instructed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,9 +6,17 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return(
+  return (
     <div>
-      {/* code here */}
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
     </div>
   )
 }
