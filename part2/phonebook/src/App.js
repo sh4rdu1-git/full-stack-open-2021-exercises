c1a4fa2c913b113286eb4a737109d34cd005016d
sh4rdu1-git <mulayshardul99@gmail.com>
2021-07-15 22:58:37 +0530

    Exercise 2.6: phonebook step1 complete
    Functionality to add new names to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  // contacts are stored in array 'persons'
+  const [persons, setPersons] = useState([
+    {
+      name: 'Arto Hellas',
+    }
+  ])
+
+  const [newName, setNewName] = useState('') // for name text input
+
+  // updates state on every change in input
+  const handleNameTextChange = (event) => setNewName(event.target.value)
+
+  // adds new contact to array 'persons'
+  const handleAddPerson = (event) => {
+    event.preventDefault()
+    const newPersonObject = {
+      id: persons.length + 1,
+      name: newName
+    }
+    setPersons(persons.concat(newPersonObject))
+    setNewName('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameTextChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleAddPerson}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {/* in above map function, we have used person's name as the key */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
