d87d988b4bb91daa2ed54ee0367ed73af1a560b8
sh4rdu1-git <mulayshardul99@gmail.com>
2021-07-15 12:02:28 +0530

    Exercise 2.4: courseinfo step9 complete
    Added one more course in the 'courses' array at the top. Modified the code to show multiple courses from the new 'courses' array.

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 // Header component
 const Header = ({ courseName }) => {
   return (
-    <h1>{courseName}</h1>
+    <h2>{courseName}</h2>
   )
 }
 
@@ -47,35 +47,60 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack Application Development',
-    parts: [
-      {
-        id: 1,
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        id: 2,
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        id: 3,
-        name: 'State of a component',
-        exercises: 14,
-      },
-      {
-        id: 4,
-        name: 'Redux',
-        exercises: 11,
-      }
-    ]
-  }
+  // Array of objects containing all courses
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack Application Development',
+      parts: [
+        {
+          id: 1,
+          name: 'Fundamentals of React',
+          exercises: 10,
+        },
+        {
+          id: 2,
+          name: 'Using props to pass data',
+          exercises: 7,
+        },
+        {
+          id: 3,
+          name: 'State of a component',
+          exercises: 14,
+        },
+        {
+          id: 4,
+          name: 'Redux',
+          exercises: 11,
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Node.js',
+      parts: [
+        {
+          id: 1,
+          name: 'Routing',
+          exercises: 3,
+        },
+        {
+          id: 2,
+          name: 'Middlewares',
+          exercises: 7,
+        },
+      ]
+    }
+  ]
 
   return (
-    <Course course={course} />
+    <>
+      <h1>Web Development Curriculum</h1>
+      {/* Map each element of the 'courses' array to <Course /> component */}
+      {courses.map((course) =>
+        <Course key={course.id} course={course} />
+      )}
+    </>
   )
 }
 
