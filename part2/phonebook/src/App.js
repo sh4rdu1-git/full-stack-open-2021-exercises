22b3828ac85d0e8863bea1a66721a614753b4653
sh4rdu1-git <mulayshardul99@gmail.com>
2021-07-16 16:34:15 +0530

    Exercise 2.8: phonebook step3 completed
    Added input for phone numbers

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,13 +5,16 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       name: 'Arto Hellas',
+      phone: '1800123123',
     }
   ])
 
   const [newName, setNewName] = useState('') // for name text input
+  const [newPhone, setNewPhone] = useState('') // for phone number input
 
   // updates state on every change in input
   const handleNameTextChange = (event) => setNewName(event.target.value)
+  const handlePhoneNumberChange = (event) => setNewPhone(event.target.value)
 
   // adds new contact to array 'persons'
   const handleAddPerson = (event) => {
@@ -25,10 +28,12 @@ const App = () => {
     } else {
       const newPersonObject = {
         id: persons.length + 1,
-        name: newName
+        name: newName,
+        phone: newPhone,
       }
       setPersons(persons.concat(newPersonObject))
       setNewName('')
+      setNewPhone('')
     }
   }
 
@@ -40,11 +45,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameTextChange} />
         </div>
         <div>
+          phone: <input type="phone" value={newPhone} onChange={handlePhoneNumberChange} />
+        </div>
+        <div>
           <button type="submit" onClick={handleAddPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name}: {person.phone}</p>)}
       {/* in above map function, we have used person's name as the key */}
     </div>
   )
